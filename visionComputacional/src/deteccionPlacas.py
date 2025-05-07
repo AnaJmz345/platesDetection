88e5702ec685c53efd70e0a3c6159d62d0bed17d
Ana Paola Jiménez Sedano <143899647+AnaJmz345@users.noreply.github.com>
2025-05-06 20:19:33 -0600

    Fix variables names
    Standarize all the variables names to snake case and assign clearer names

--- visionComputacional/src/deteccionPlacas.py
+++ visionComputacional/src/deteccionPlacas.py
@@ -4,93 +4,93 @@ import easyocr
 
 
 #Lee la imagen
-p1 = cv.imread("../assets/placa_q.jpg")
+plate_1 = cv.imread("../assets/placa_q.jpg")
 #Cambia el color de la imagen a una con colores grises
-p1 = cv.cvtColor(p1, cv.COLOR_BGR2GRAY)
-cv.imshow("Placa 1 gris",p1)
+plate_1 = cv.cvtColor(plate_1, cv.COLOR_BGR2GRAY)
+cv.imshow("Placa 1 gris",plate_1)
 #Aplica un desenfoque con el filtro gaussianox
-p1=cv.GaussianBlur(src=p1, ksize=(19,19), sigmaX=43)
-cv.imshow("Placa 1 desenfocada",p1)
+plate_1=cv.GaussianBlur(src=plate_1, ksize=(19,19), sigmaX=43)
+cv.imshow("Placa 1 desenfocada",plate_1)
 #aplica un detector de  bordes
-edges1 =cv.Canny(p1, 80, 122)
-cv.imshow("Bordes placa 1",edges1)
+edges_1 =cv.Canny(plate_1, 80, 122)
+cv.imshow("Bordes placa 1",edges_1)
 
 
 #detección de texto del número de placa con easyocr
 reader = easyocr.Reader(['es', 'en'])
-results = reader.readtext(p1)
+results = reader.readtext(plate_1)
 text =""
 
-for detectedText in results:
-  text += detectedText[1]+ "\n"
+for detected_text in results:
+  text += detected_text[1]+ "\n"
 print("\nTexto detectado en la placa 1:",text)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
 
 #Lee la imagen
-p2 = cv.imread("../assets/placa_2.jpg")
+plate_2 = cv.imread("../assets/placa_2.jpg")
 #Cambia el color de la imagen a una con colores grises
-p2 = cv.cvtColor(p2, cv.COLOR_BGR2GRAY)
-cv.imshow("Placa 2 gris",p2)
+plate_2 = cv.cvtColor(plate_2, cv.COLOR_BGR2GRAY)
+cv.imshow("Placa 2 gris",plate_2)
 #Aplica un desenfoque con el filtro gaussiano
-p2=cv.GaussianBlur(src=p2, ksize=(121,121), sigmaX=0) #101
-cv.imshow("Placa 2 desenfocada",p2)
+plate_2=cv.GaussianBlur(src=plate_2, ksize=(121,121), sigmaX=0) #101
+cv.imshow("Placa 2 desenfocada",plate_2)
 #aplica un detector de  bordes
-edges2 =cv.Canny(p2, 1,25)
-cv.imshow("Bordes placa 2",edges2)
+edges_2 =cv.Canny(plate_2, 1,25)
+cv.imshow("Bordes placa 2",edges_2)
 
 #detección de texto del número de placa con easyocr
 reader = easyocr.Reader(['es', 'en'])
-results2 = reader.readtext(p2)
-text2 =""
-for detectedText2 in results2:
-  text2 += detectedText2[1]+ "\n"
-print("\nTexto detectado en la placa:",text2)
+results_2 = reader.readtext(plate_2)
+text_2 =""
+for detected_text_2 in results_2:
+  text_2 += detected_text_2[1]+ "\n"
+print("\nTexto detectado en la placa:",text_2)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
 
 
 #Lee la imagen
-p3 = cv.imread("../assets/placa_3.jpg")
+plate_3 = cv.imread("../assets/placa_3.jpg")
 #Cambia el color de la imagen a una con colores grises
-p3 = cv.cvtColor(p3, cv.COLOR_BGR2GRAY)
-cv.imshow("Placa 3 gris",p3)
+plate_3 = cv.cvtColor(plate_3, cv.COLOR_BGR2GRAY)
+cv.imshow("Placa 3 gris",plate_3)
 #Aplica un desenfoque con el filtro gaussiano
-p3=cv.GaussianBlur(src=p3, ksize=(25,25), sigmaX=0)
-cv.imshow("Placa 3 desenfocada",p3)
+plate_3=cv.GaussianBlur(src=plate_3, ksize=(25,25), sigmaX=0)
+cv.imshow("Placa 3 desenfocada",plate_3)
 #aplica un detector de  bordes
-edges3 =cv.Canny(p3, 100,120)
-cv.imshow("Bordes placa 3",edges3)
+edges_3 =cv.Canny(plate_3, 100,120)
+cv.imshow("Bordes placa 3",edges_3)
 
 #detección de texto del número de placa con easyocr
 reader = easyocr.Reader(['es', 'en'])
-results3 = reader.readtext(p3)
-text3 =""
-for detectedText3 in results3:
-  text3 += detectedText3[1]+ "\n"
-print("\nTexto detectado en la placa:",text3)
+results_3 = reader.readtext(plate_3)
+text_3 =""
+for detected_text_3 in results_3:
+  text_3 += detected_text_3[1]+ "\n"
+print("\nTexto detectado en la placa:",text_3)
 
 
 cv.waitKey(0)
 cv.destroyAllWindows()
 
 #Lee la imagen
-p4 = cv.imread("../assets/placa_4.jpg")
+plate_4 = cv.imread("../assets/placa_4.jpg")
 #Cambia el color de la imagen a una con colores grises
-p4 = cv.cvtColor(p4, cv.COLOR_BGR2GRAY)
-cv.imshow("Placa 4 gris",p4)
+plate_4 = cv.cvtColor(plate_4, cv.COLOR_BGR2GRAY)
+cv.imshow("Placa 4 gris",plate_4)
 #Aplica un desenfoque con el filtro gaussiano
-p4=cv.GaussianBlur(src=p4, ksize=(89,89), sigmaX=0)
-cv.imshow("Placa 4 desenfoque",p4)
+plate_4=cv.GaussianBlur(src=plate_4, ksize=(89,89), sigmaX=0)
+cv.imshow("Placa 4 desenfoque",plate_4)
 #aplica un detector de  bordes
-edges4 =cv.Canny(p4, 40,45)
-cv.imshow("Bordes placa 4",edges4)
+edges_4 =cv.Canny(plate_4, 40,45)
+cv.imshow("Bordes placa 4",edges_4)
 #detección de texto del número de placa con easyocr
 reader = easyocr.Reader(['es', 'en'])
-results4 = reader.readtext(p4)
-text4 =""
-for detectedText4 in results4:
-  text4 += detectedText4[1]+ "\n"
-print("\nTexto detectado en la placa:",text4)
+results_4 = reader.readtext(plate_4)
+text_4 =""
+for detected_text_4 in results_4:
+  text_4 += detected_text_4[1]+ "\n"
+print("\nTexto detectado en la placa:",text_4)
