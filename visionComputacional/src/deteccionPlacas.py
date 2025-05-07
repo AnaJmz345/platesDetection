7ad7ef0b80090f7e580205f71be23bfff51d8c0e
Ana Paola Jiménez Sedano <143899647+AnaJmz345@users.noreply.github.com>
2025-05-06 21:18:12 -0600

    Add comments to all the code

--- visionComputacional/src/deteccionPlacas.py
+++ visionComputacional/src/deteccionPlacas.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import easyocr
 
+#Images list with its path, the kernel size for blurring and the limits for canny
 images = [
     {"path": "../assets/placa_q.jpg", "ksize": (19, 19), "sigmaX": 43, "canny_lower": 80,  "canny_upper": 122},
     {"path": "../assets/placa_2.jpg", "ksize": (121,121), "sigmaX": 0,  "canny_lower": 1,   "canny_upper": 25},
@@ -8,36 +9,40 @@ images = [
     {"path": "../assets/placa_4.jpg", "ksize": (89, 89),  "sigmaX": 0,  "canny_lower": 40,  "canny_upper": 45},
 ]
 
+#Creates an instancy of the OCR lector in english and spanish
 reader = easyocr.Reader(['es', 'en'])
 
+#Does all the plate processing
 def get_plate(plate_details,plate_number):
-  #Lee la imagen
+
+  #Read the image
   plate = cv.imread(plate_details["path"])
   if plate is None:
     print(f"No se pudo cargar la imagen: {plate_details['path']}")
     return
 
-
-  #Cambia el color de la imagen a una con colores grises
+  #Changes the image colors to a gray scale
   plate = cv.cvtColor(plate, cv.COLOR_BGR2GRAY)
 
-  #Aplica un desenfoque con el filtro gaussianox
-  plate=cv.GaussianBlur(src=plate, ksize=plate_details["ksize"], sigmaX=plate_details["sigmaX"])
-  cv.imshow(f"Plate {plate_number} blurred",plate)
-  #aplica un detector de  bordes
-  edges =cv.Canny(plate, plate_details["canny_lower"], plate_details["canny_upper"],)
+  #Blurs the image with a gaussian filter
+  plate = cv.GaussianBlur(src=plate, ksize=plate_details["ksize"], sigmaX=plate_details["sigmaX"])
+  cv.imshow(f"Plate {plate_number} blurred", plate)
+
+  #Applies a border detector
+  edges = cv.Canny(plate, plate_details["canny_lower"], plate_details["canny_upper"])
   cv.imshow(f"Plate {plate_number} borders",edges)
 
-  #detección de texto del número de placa con easyocr
+  #Plate number detection text with ocr
   results = reader.readtext(plate)
-  text =""
-  
+  text = ""
   for detected_text in results:
-    text += detected_text[1]+ "\n"
+    text += detected_text[1]+"\n"
   print(f"\n Plate {plate_number} detected text: ",text)
 
+  #Closes the 2 shown windows when any key is pressed
   cv.waitKey(0)
   cv.destroyAllWindows()
 
+#Loop to process all the plates
 for plate_number,plate_details in enumerate(images,start=1):
   get_plate(plate_details,plate_number)
